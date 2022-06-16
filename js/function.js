39914cf0237ac95459826afd1330f485296e839b
yu405405 <66393193+you405405@users.noreply.github.com>
2022-06-17 08:10:01 +0900

    【記録用】コードレビュー前の自分で書いたコード

--- js/function.js
+++ js/function.js
@@ -16,57 +16,61 @@ const changeActive = function() {
 const cell = document.querySelectorAll('.cell')
 let isTurnMaru = true
 let clickCount = 0
-
-
-
-// 勝ちパターンを定義
-let winPattern = [
-    [0, 1, 2],
-    [0, 3, 6],
-    [0, 4, 8],
-    [3, 4, 5],
-    [6, 7, 8],
-    [1, 4, 7],
-    [2, 5, 8],
-    [2, 4, 6]
-]
-
-
-const checkWinPatterns = function() {
-    for(let i = 0; i < winPattern.length; i++) {
-        const [first, second, third] = winPattern[i]
-        
-        // マス目の状態が勝ちパターンと一致した時にゲーム終了
-        if(cell[first].textContent !== "" && cell[first].textContent === cell[second].textContent && cell[first].textContent === cell[third].textContent) {
-            return true
-        }
-    }
-
-    return false
-}
 cell.forEach(function(element) {
     element.addEventListener('click', function(e) {
-        if (element.textContent !== '') {
-            return
+        // ○と×交互に表示する（isTurnMaruがtrueの時は○・falseの時は×のターン）
+        if(isTurnMaru && element.textContent === '') {
+            element.textContent = '○'
+            isTurnMaru = false
+            changeActive()
+            //既に○×が追加されているところはclickできないようにする
+            element.classList.add('click_none')
+        } else if(isTurnMaru === false && element.textContent === '') {
+            element.textContent = '×'
+            isTurnMaru = true
+            changeActive()
+            element.classList.add('click_none')
         }
-
-        element.textContent = isTurnMaru ? '○' : '×'
-        isTurnMaru = !isTurnMaru
-        changeActive()
-        
         // クリックカウント数を計算
         clickCount++
+        
 
         // 勝ちパターンとマス目の比較判定をする
-        const done = checkWinPatterns()
-        if (done) {
-            comment.textContent = isTurnMaru ? 'x win' : '◯ win'
-            box.classList.add('click_none')
-        } else if (clickCount === 9) {
-            comment.textContent = 'draw'
+        const clickFunction = function() {
+            // 勝ちパターンを定義
+            let winPattern = [
+                [0, 1, 2],
+                [0, 3, 6],
+                [0, 4, 8],
+                [3, 4, 5],
+                [6, 7, 8],
+                [1, 4, 7],
+                [2, 5, 8],
+                [2, 4, 6]
+            ]
+            
+            for(let i = 0; i < winPattern.length; i++) {
+                let winPattern0 = winPattern[i][0]
+                let winPattern1 = winPattern[i][1]
+                let winPattern2 = winPattern[i][2]
+                
+                // マス目の状態が勝ちパターンと一致した時にゲーム終了
+                if(cell[winPattern0].textContent === '○' && cell[winPattern1].textContent === '○' && cell[winPattern2].textContent === '○') {
+                    comment.textContent = '○ win!!'
+                    //勝敗決定時以降はclick不可
+                    box.classList.add('click_none')
+                    return
+                } else if(cell[winPattern0].textContent === '×' && cell[winPattern1].textContent === '×' && cell[winPattern2].textContent === '×') {
+                    comment.textContent = '× win!!'
+                    box.classList.add('click_none')
+                    return
+                }
+            }
+            if(clickCount === 9 && element.textContent !== '') {
+                comment.textContent = 'draw'
+            }
         }
-        
-        
+        clickFunction()
     })
 })
 
